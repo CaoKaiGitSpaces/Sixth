c1cc6986289d4430f27ef5fe75c2e78fc1c04ea2
CaoKaiSpapes <ck_182@.com>
2015-04-24 11:36:36 +0800

    Initial Commit

---
+++ Sixth/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Sixth
+//
+//  Created by 曹凯 on 15/4/24.
+//  Copyright (c) 2015年 CK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Sixth/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Sixth
+//
+//  Created by 曹凯 on 15/4/24.
+//  Copyright (c) 2015年 CK. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
